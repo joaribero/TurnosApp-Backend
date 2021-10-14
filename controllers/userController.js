09dd8bb6d9596d5599e0f5e7fc89e315673b47a7
Joaribero <joaribero_la12@hotmail.com>
2021-10-13 21:25:07 -0300

    Admin control and helper function

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,25 +2,38 @@ const Roles = require('../models/roles');
 const User = require('../models/user');
 const State = require('../models/userState');
 
+const isAdmin = async (id) => {
+    const user = await User.findById(id);
+    if (user) {
+        if (user.roleId === 1) {
+            return {admin: true, error: null, category: null}
+        } else return {admin: false, error: 'No posees los permisos necesarios para realizar esta acción.', category: 'error'};
+    }
+    return {admin: false, error: 'Acceso denegado', category: 'error'}; 
+}
+
 const ctrl = {};
 
 ctrl.createRole = async (req, res) => {
     
-    if(!req.user) {
-        res.send({msg:'Acceso denegado', category:'error'});
-    } else if (req.user.roleId === 1) {
-        
-        const count = (await Roles.find()).length;
+    //controlo que llegue un usuario en el rq
+    if(req.user) {
+        const {admin, error, category} = isAdmin(req.user.id);
+        if (error) {
+            res.send({error, category});
+        } else {
+            const count = (await Roles.find()).length;
 
-        let role = new Roles;
-        role.Id = count + 1;
-        role.Name = req.body.name;
-        role.Description = req.body.description
+            let role = new Roles;
+            role.Id = count + 1;
+            role.Name = req.body.name;
+            role.Description = req.body.description
 
-        await role.save();
-        res.send({msg: 'Rol creado', category: 'success'});
+            await role.save();
+            res.send({msg: 'Rol creado', category: 'success'});
+        }  
     } else {
-        res.send({msg: 'No posees los permisos necesarios para realizar esta acción.', category: 'error'});
+        res.send({error:'Acceso denegado', category:'error'});
     }    
 }
 
@@ -29,24 +42,27 @@ ctrl.getRoles = async (req, res) => {
     res.send(roles);
 }
 
-ctrl.createState = async (req, res) => {
+ctrl.createState = async (req, res) => {    
     
-    if(!req.user) {
-        res.send({msg:'Acceso denegado', category:'error'});
-    } else if (req.user.roleId === 1) {
-        
-        const count = (await State.find()).length;
+    //controlo que llegue un usuario en el rq
+    if(req.user) {
+        const {admin, error, category} = isAdmin(req.user.id);
+        if (error) {
+            res.send({error, category});
+        } else {
+            const count = (await State.find()).length;
 
-        let state = new State;
-        state.Id = count + 1;
-        state.Name = req.body.name;
-        state.Description = req.body.description
+            let state = new State;
+            state.Id = count + 1;
+            state.Name = req.body.name;
+            state.Description = req.body.description
 
-        await state.save();
-        res.send({msg: 'Estado creado', category: 'success'});
+            await state.save();
+            res.send({msg: 'Estado creado', category: 'success'});
+        }  
     } else {
-        res.send({msg: 'No posees los permisos necesarios para realizar esta acción.', category: 'error'});
-    }    
+        res.send({error:'Acceso denegado', category:'error'});
+    }   
 }
 
 ctrl.getUserStates = async (req, res) => {
@@ -55,9 +71,37 @@ ctrl.getUserStates = async (req, res) => {
 }
 
 ctrl.users = async (req, res) => {
-    const users = await User.find();
 
-    res.send(users);
+    //controlo que llegue un usuario en el rq
+    if(req.user) {
+        const {admin, error, category} = isAdmin(req.user.id);
+        if (error) {
+            res.send({error, category});
+        } else {
+            const users = await User.find();
+            res.send(users);
+        }
+
+    } else {
+        res.send({error:'Acceso denegado', category:'error'});
+    } 
+    
 };
+
+ctrl.setRoleAndState = async (req, res) => {
+    //controlo que llegue un usuario en el rq
+    if(req.user) {
+        const {admin, error, category} = isAdmin(req.user.id);
+        if (error) {
+            res.send({error, category});
+        } else {
+            
+            //TODO
+        }
+
+    } else {
+        res.send({error:'Acceso denegado', category:'error'});
+    } 
+}
 
 module.exports = ctrl;
